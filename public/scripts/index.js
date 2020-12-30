0b6d9f84193e58cb930339e2946ce0aae86a7b12
Doopdon <gannonhuiting@gmail.com>
2020-12-30 00:07:26 -0700

    added dope stars

---
+++ public/scripts/index.js
@@ -0,0 +1,56 @@
+(function () {
+    ['about-me', 'experience','projects','about-this-site','contact'].forEach(function (name) {
+      document.getElementById(name + '-link').onclick = function () { scrollTo(name) };
+    });
+    function scrollTo(elemId) {
+      var $elem = document.getElementById(elemId);
+      $elem.scrollIntoView({ behavior: "smooth"});
+    }
+  })();
+
+
+  let body = document.getElementsByTagName('body')[0];
+    let buttons = document.getElementsByClassName('debug');
+    buttons[0].onclick = off;
+    buttons[1].onclick = on;
+    function on() {
+      body.classList.add('debug-on');
+    }
+    function off() {
+      body.classList.remove('debug-on');
+    }
+
+
+function makeStars(){
+   let $inner = document.getElementsByClassName('inner')[0]
+   
+    let size = ''
+    loop(50,makeStar)
+    size = 'small'
+    loop(100,makeStar)
+    size = 'big'
+    loop(20,makeStar)
+   
+    function makeStar(){
+        let $elem = document.createElement('div');
+        $elem.classList.add('star');
+        size && $elem.classList.add(size);
+        $elem.style = `right:${Math.random()*99}%; top:${randBiased(100)}%;`
+        $inner.appendChild($elem)
+    }
+}
+makeStars();
+
+function loop(int,callBack){
+    for(let i =0; i < int; i++)
+        callBack(i);
+}
+
+function randBiased(num){
+    return (1-Math.sqrt(1-Math.random()))*num + 1;
+}
+
+
+    
+
+
